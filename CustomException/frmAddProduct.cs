3ea2a350a7b578ef15873ff601f4e1379d0b664d
Janel Galisfrancisco <janelfrancisco2122@gmail.com>
2025-09-15 21:03:39 +0800

    methods copied from file and Exception was added

--- CustomException/frmAddProduct.cs
+++ CustomException/frmAddProduct.cs
@@ -5,6 +5,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Text;
+using System.Text.RegularExpressions;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
@@ -17,9 +18,33 @@ namespace CustomException
         private int _Quantity;
         private double _Price;
 
+
+
         public frmAddProduct()
         {
             InitializeComponent();
+        }
+
+        // 8. Copy the methods in 03 Laboratory Exercise 2 – Method.txt file and paste it in the frmAddProduct class.
+        // Exception added
+
+        public string Product_Name(string name)
+        {
+            if (!Regex.IsMatch(name, @"^[a-zA-Z]+$"))
+                throw new ArgumentException("Product name must only contain letters.");
+            return name;
+        }
+        public int Quantity(string qty)
+        {
+            if (!Regex.IsMatch(qty, @"^[0-9]"))
+                throw new ArgumentException("Quantity must only contain numbers.");
+            return Convert.ToInt32(qty);
+        }
+        public double SellingPrice(string price)
+        {
+            if (!Regex.IsMatch(price.ToString(), @"^(\d*\.)?\d+$"))
+                throw new ArgumentException("Selling price must be a valid number.");
+            return Convert.ToDouble(price);
         }
 
         private void label3_Click(object sender, EventArgs e)
@@ -41,7 +66,7 @@ namespace CustomException
         // Create a string array named ListOfProductCategory.
         private void frmAddProduct_Load(object sender, EventArgs e)
         {
-            string[] ListOfProgram = new string[] {
+            string[] ListOfProductCategory = new string[] {
                 "Beverages",
                 "Bread/Bakery",
                 "Canned/Jarred Goods",
@@ -51,6 +76,12 @@ namespace CustomException
                 "Personal Care",
                 "Other"
             };
+
+            // 7. Write a foreach statement to show the data of the created string array inside the ComboBox
+            foreach (string category in ListOfProductCategory)
+            {
+                cbCategory.Items.Add(category);
+            }
 
 
 
